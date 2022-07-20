f40ca7c43677ce20bc25a5c3f2e3704eb747ce68
xxidk82xx <IiXxcjackxXiI@outlook.com>
2022-07-20 13:49:35 -0600

    implimented an early version of the edit function for display pourposes

--- commands/edit.js
+++ commands/edit.js
@@ -1,16 +1,53 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, SelectMenuBuilder, ActionRowBuilder } = require('@discordjs/builders');
 const { ReactionUserManager } = require('discord.js');
+const nationList = require("../nations.json");
 
-module.exports = {
+module.exports = 
+{
 	data: new SlashCommandBuilder()
-		.setName('edit')
-		.setDescription('edits nation stats'),
-	async execute(interaction) {
-		if (interaction.user.id !== "518634527267225620")
+	.setName('edit')
+	.setDescription('edits nation stats'),		
+	
+	async execute(interaction) 
+	{
+		
+		let menuContents = [{}]
+		let length = 0;
+		nationList.forEach(db => 
 		{
-			interaction.reply({ content: "not allowed to use this command", fetchReply: true })
-			return
-		}
-
+			if( length == 0)
+			{
+				menuContents[0] = 
+				{
+					label: db.name,
+					description: ' ',
+					value: db.id.toString()
+				}
+			}
+			else
+			{
+				menuContents.push
+				(
+					{
+						label: db.name,
+						description: ' ',
+						value: db.id.toString()
+					}
+				)
+			}
+			length++
+		});
+		var ActionRow = new ActionRowBuilder()
+			.addComponents
+			(
+				new SelectMenuBuilder()
+					.setMinValues(1)
+					.setMaxValues(1)
+					.setPlaceholder("select a nation")
+					.setCustomId("nationSelection")
+					.setOptions(menuContents)
+			)
+		await interaction.reply({components: [ActionRow]});
+		
 	},
 };
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const discord = require ("discord.js")
+const { SelectMenuBuilder, ActionRowBuilder } = require('@discordjs/builders');
 const path = require('node:path');
 const fs = require("fs");
 const { Client, Intents, Collection } = require('discord.js');
 const {token} = require('./config.json');
 const client = new Client({ intents: ["Guilds", "GuildMessages"] });
 client.commands = new Collection();
+const nationList = require("./nations.json");
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -21,17 +23,52 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+	if (interaction.isChatInputCommand())
+	{
+		const command = client.commands.get(interaction.commandName);
 
-	const command = client.commands.get(interaction.commandName);
+		if (!command) return;
 
-	if (!command) return;
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+	else if (interaction.isSelectMenu())
+	{
+		if(interaction.customId === "nationSelection")
+		{
+			let selection = nationList[parseInt(interaction.values)]
+			var ActionRow = new ActionRowBuilder()
+				.addComponents
+				(
+					new SelectMenuBuilder()
+						.setMinValues(1)
+						.setMaxValues(1)
+						.setPlaceholder("now select a value")
+						.setCustomId("valueSelection")
+						.setOptions(
+							[
+								{
+									label: `stability: ${selection.stability}`,
+									description: "the stability of the nation",
+									value: "stability"
+								},
+								{
+									label: `stability: ${selection.warSupport}`,
+									description: "the war support of the nation",
+									value: "war support"
+								}
+							])
+				)
+			interaction.update({content: `you selected ${selection.name}`, components: [ActionRow]})
+		}
+		else if(interaction.customId === "valueSelection")
+		{
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	}
 });
 client.login(token);
